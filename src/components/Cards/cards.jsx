992e4a0b014224d3561e93691cc32c1b046e5a28
famgz <famgz@proton.me>
2023-12-03 18:52:11 -0300

    feat: cards structure

---
+++ src/components/Cards/cards.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import styles from './Cards.module.scss'
+
+
+const Cards = ({ results }) => {
+
+    let display
+
+    // Badge color
+    let badgeColor = (status) => {
+        switch (status) {
+            case 'Alive':
+                return 'bg-success'
+            case 'Dead':
+                return 'bg-danger'
+            default:
+                return 'bg-secondary'
+        }
+    }
+
+    if (!results) {
+        return (
+            <>No characters found :('</>
+        )
+    }
+
+    return (
+        results.map(item => {
+            let { id, name, image, location, status } = item
+            return (
+                <div key={id} className="d-flex col-4 mb-4 position-relative">
+                    <div className={`${styles.cards} d-flex flex-column`}>
+                        {/* Image */}
+                        <img src={image} alt="" className="img-fluid" />
+                        {/* Info */}
+                        <div className="content p-2 d-flex flex-column flex-grow-1 justify-content-between">
+                            <div className="fs-4 fw-bold mb-2">{name}</div>
+                            <div className="">
+                                <div className="fs-6">Last location</div>
+                                <div className="fs-5 text-truncate">{location.name}</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Badge */}
+                    <div className={`badge ${badgeColor(status)} position-absolute ${styles.badge}`}>
+                        {status}
+                    </div>
+                </div>
+            )
+        })
+    )
+}
+
+export default Cards
