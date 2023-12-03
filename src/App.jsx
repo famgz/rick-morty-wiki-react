fe4686b4aad25efe9f7a2c7887d6e847ffe70e7e
famgz <famgz@proton.me>
2023-12-03 10:59:46 -0300

    build: project config and structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1 className="text-center ubuntu my-5">
+        Rick & Morty <span className="text-primary">Wiki</span>
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;
