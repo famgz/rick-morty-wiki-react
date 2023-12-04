dca4525460cc47ba545a49cee2a52543b92758e3
famgz <famgz@proton.me>
2023-12-04 13:11:47 -0300

    feat: pagination

--- src/components/Pagination/pagination.jsx
+++ src/components/Pagination/pagination.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './Pagination.module.scss'
 
 const Pagination = ({ pageNumber, setPageNumber, pages }) => {
 
@@ -12,10 +13,21 @@ const Pagination = ({ pageNumber, setPageNumber, pages }) => {
         setPageNumber(x => x + 1)
     }
 
+    let prevColor = () => {
+        let style = pageNumber === 1 ? styles['btn-deactivate'] : 'btn-primary'
+        return style
+
+    }
+
+    let nextColor = () => {
+        let style = pageNumber >= pages ? styles['btn-deactivate'] : 'btn-primary' 
+        return style
+    }
+
     return (
         <div className='container d-flex justify-content-center gap-5 my-5'>
-            <button onClick={prev} className="btn btn-primary">Previous</button>
-            <button onClick={next} className="btn btn-primary">Next</button>
+            <button onClick={prev} className={`btn ${prevColor()}`}>Previous</button>
+            <button onClick={next} className={`btn ${nextColor()}`}>Next</button>
         </div>
     )
 }
