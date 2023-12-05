9919a2fa12d10e0464bf8cb9d18f091f7d548405
famgz <famgz@proton.me>
2023-12-05 17:47:35 -0300

    refactor: extracted Home to Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import React, { useState, useEffect } from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
-import Filters from './components/Filters/Filters';
-import Cards from './components/Cards/Cards';
-import Pagination from './components/Pagination/Pagination';
-import Search from './components/Search/Search';
-import Navbar from './components/Navbar/Navbar';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar';
+import Home from './Pages/Home';
 import Episodes from './Pages/Episodes';
 import Locations from './Pages/Locations';
 
@@ -19,76 +15,12 @@ function App() {
                 <Navbar />
             </div>
             <Routes>
-                <Route path='/' element={<Home/>} />
-                <Route path='/episodes' element={<Episodes/>} />
-                <Route path='/locations' element={<Locations/>} />
+                <Route path='/' element={<Home />} />
+                <Route path='/episodes' element={<Episodes />} />
+                <Route path='/locations' element={<Locations />} />
             </Routes>
         </Router>
     );
 }
-
-
-// function App() {  // before Route
-const Home = () => {
-    let [pageNumber, setPageNumber] = useState(1); // (1) is the default value
-    let [search, setSearch] = useState('');
-    let [status, setStatus] = useState('');
-    let [species, setSpecies] = useState('');
-    let [gender, setGender] = useState('');
-    let [type, setType] = useState('');
-    let [fetchedData, updateFetchedData] = useState([]);
-    let { info, results } = fetchedData; // Destructure
-
-    let apiUrl =
-        'https://rickandmortyapi.com/api/character?' +
-        `page=${pageNumber}` +
-        `&name=${search}` +
-        `&status=${status}` +
-        `&species=${species}` +
-        `&gender=${gender}` +
-        `&type=${type}`;
-
-    useEffect(() => {
-        // IIFE -> Immediately Invoked Function Expression
-        (async function () {
-            let data = await fetch(apiUrl).then((res) => res.json());
-            console.log(data);
-            updateFetchedData(data);
-        })();
-    }, [apiUrl]);
-
-    return (
-        <div className='App'>
-            <Search setPageNumber={setPageNumber} setSearch={setSearch} />
-
-            <div className='container'>
-                {/* In bootstrap there are 12 columns inside a row */}
-                <div className='row'>
-                    {/* Filter tab */}
-                    <Filters
-                        setPageNumber={setPageNumber}
-                        setStatus={setStatus}
-                        setSpecies={setSpecies}
-                        setGender={setGender}
-                        setType={setType}
-                    />
-
-                    {/* Cards grid */}
-                    <div className='col-8'>
-                        <div className='row'>
-                            <Cards results={results} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <Pagination
-                info={info}
-                pageNumber={pageNumber}
-                setPageNumber={setPageNumber}
-            />
-        </div>
-    );
-};
 
 export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import Filters from '../components/Filters/Filters';
+import Cards from '../components/Cards/Cards';
+import Pagination from '../components/Pagination/Pagination';
+import Search from '../components/Search/Search';
+
+
+const Home = () => {
+    let [pageNumber, setPageNumber] = useState(1); // (1) = default value
+    let [search, setSearch] = useState('');
+    let [status, setStatus] = useState('');
+    let [species, setSpecies] = useState('');
+    let [gender, setGender] = useState('');
+    let [type, setType] = useState('');
+    let [fetchedData, updateFetchedData] = useState([]);
+    let { info, results } = fetchedData; // it's called destructure
+
+    let apiUrl =
+        'https://rickandmortyapi.com/api/character?' +
+        `page=${pageNumber}` +
+        `&name=${search}` +
+        `&status=${status}` +
+        `&species=${species}` +
+        `&gender=${gender}` +
+        `&type=${type}`;
+
+    useEffect(() => {
+        // IIFE -> Immediately Invoked Function Expression
+        (async function () {
+            let data = await fetch(apiUrl).then((res) => res.json());
+            console.log(data);
+            updateFetchedData(data);
+        })();
+    }, [apiUrl]);
+
+    return (
+        <div className='App'>
+            <Search setPageNumber={setPageNumber} setSearch={setSearch} />
+
+            <div className='container'>
+                {/* In bootstrap there are 12 columns inside a row */}
+                <div className='row'>
+                    {/* Filter tab */}
+                    <Filters
+                        setPageNumber={setPageNumber}
+                        setStatus={setStatus}
+                        setSpecies={setSpecies}
+                        setGender={setGender}
+                        setType={setType}
+                    />
+
+                    {/* Cards grid */}
+                    <div className='col-8'>
+                        <div className='row'>
+                            <Cards results={results} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Pagination
+                info={info}
+                pageNumber={pageNumber}
+                setPageNumber={setPageNumber}
+            />
+        </div>
+    );
+};
+
+export default Home;
